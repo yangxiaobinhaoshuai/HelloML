481f96560fe2c1fdac021e2b31f41f5db9768bf5
yangxiaobin01 <yangxiaobin01@zuoyebang.com>
2025-12-16 17:44:08 +0800

    Initial commit.

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,14 @@
+
+
+
+import numpy as np
+
+str = 'python'
+print(str[-1])
+print(str[-2])
+print(str[-3])
+print(str[-4])
+print(str[-5])
+print(str[-6])
+# print("np.version.version: ", np.version.version)
+# print("Linear Regression Model")
