0c29de209790ee373f05b5541bb41db789d2eff9
yangxiaobin01 <yangxiaobin01@zuoyebang.com>
2025-12-18 22:18:20 +0800

    Updates in 2025.12.18.

--- linear_regression/numpy/linear_regression.py
+++ linear_regression/numpy/linear_regression.py
@@ -19,6 +19,7 @@ noise = np.random.normal(loc=0, scale=0.01, size=labels.shape).astype(np.float32
 
 print("noise shape: ", noise.shape)
 
+
 # labels += noise
 
 
@@ -32,6 +33,33 @@ def set_figsize(figsize=(6, 4)):
     plt.rcParams["figure.figsize"] = figsize
 
 
+def data_iter(batch_size, features, labels):
+    nums = len(features)
+    indices = list(range(nums))
+    np.random.shuffle(indices)
+    for i in range(0, nums, batch_size):
+        batch_idx = indices[i:min(i + batch_size, nums)]
+        features_batch = features[batch_idx]
+        labels_batch = labels[batch_idx]
+        yield features_batch, labels_batch
+
+
+w = np.random.normal(loc=0, scale=0.01, size=(num_input, 1)).astype(np.float32)
+b = np.zeros((1,), dtype=np.float32)
+
+
+def linear_reg(X, w, b):
+    return np.dot(X, w) + b
+
+
+def squared_loss(y_true, y_hat):
+    y_true = y_true.reshape(-1, 1)
+    return (y_true - y_hat) ** 2 / 2
+
+
+lr = 0.01
+batch_size = 100
+
 # Only one dimension
 set_figsize()
 plt.scatter(x=features[:, 0], y=labels, s=10, label="features")
@@ -39,4 +67,3 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
 plt.show()
-
